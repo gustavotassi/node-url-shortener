752efb0a53b58f57c2c5783c4b71a053b49f4adf
gustavotassi <souzagustavotassi@gmail.com>
2021-08-25 21:38:20 -0300

    feat: add fetch url route

--- src/routes/redirect.ts
+++ src/routes/redirect.ts
@@ -1,5 +1,27 @@
 import express from 'express';
+import { UrlService } from '../services/url-service';
 
 export const redirectRouter = express.Router();
+const urlService = new UrlService();
 
-redirectRouter.get('/:code', async (request, response) => {});
+redirectRouter.get('/:code', async (request, response) => {
+    const { code } = request.params;
+
+    try {
+        const url = await urlService.findByLongUrl(code);
+
+        if (!url) {
+            return response.status(404).json({
+                error: 'Short URL not found!',
+            });
+        }
+
+        console.log(url);
+
+        return response.redirect('url');
+    } catch (error) {
+        return response.status(400).json({
+            error: 'Failed to fetch short URL!',
+        });
+    }
+});

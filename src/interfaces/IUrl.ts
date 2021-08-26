0bedc443583b88129a9999a90b28ab54816e9b8c
gustavotassi <souzagustavotassi@gmail.com>
2021-08-25 21:37:53 -0300

    feat: add strongly typed url schema

---
+++ src/interfaces/IUrl.ts
@@ -0,0 +1,13 @@
+/**
+ * @typedef {{name: string}} IUrl
+ * @param {{name: string}} id - Registry unique ID
+ * @param {{name: string}} longUrl - Long URL (before shortening)
+ * @param {{name: string}} shortUrl - Short URL (after shortening)
+ * @param {{name: string}} date - Creation date
+ */
+export interface IUrl {
+    id: string;
+    longUrl: string;
+    shortUrl: string;
+    date: string;
+}
--- src/models/url-model.ts
+++ src/models/url-model.ts
@@ -1,13 +1,15 @@
 import mongoose from '../config/database';
+import { IUrl } from '../interfaces';
 
 const schema = new mongoose.Schema({
-    id: String,
-    longUrl: String,
-    shortUrl: String,
+    id: { type: String, required: true },
+    longUrl: { type: String, required: true },
+    shortUrl: { type: String, required: true },
     date: {
         type: Date,
+        required: true,
         default: Date.now,
     },
 });
 
-export const model = mongoose.model('Url', schema);
+export const urlModel = mongoose.model<IUrl>('Url', schema);

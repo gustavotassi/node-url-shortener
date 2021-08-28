13218974acbe1ef093c16e6a84fd3f8940b85d7f
gustavotassi <souzagustavotassi@gmail.com>
2021-08-27 23:07:47 -0300

    feat: add cors to server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
+import cors from 'cors';
 import express, { json } from 'express';
 import environment from './config/environment';
 import { redirectRouter, urlRouter } from './routes';
 
 const app = express();
 const { port } = environment;
+
+app.use(cors());
 
 app.use(json());
 
